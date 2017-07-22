210bf46d45d18768c21153c47e2bbe4d574d3b4c
Tse Kit Yam <me@kytse.com>
2017-07-22 23:30:39 +0800

    add states of bricks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,24 @@ angular
   .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/');
 
+    $stateProvider.state('redBricks', {
+      url: '/bricks/red',
+      templateUrl: 'views/bricks.html',
+      controller: 'RedBricksCtrl as brick'
+    });
+
+    $stateProvider.state('blueBricks', {
+      url: '/bricks/blue',
+      templateUrl: 'views/bricks.html',
+      controller: 'BlueBricksCtrl as brick'
+    });
+
+    $stateProvider.state('greenBricks', {
+      url: '/bricks/green',
+      templateUrl: 'views/bricks.html',
+      controller: 'GreenBricksCtrl as brick'
+    });
+
     $stateProvider.state('cart', {
       url: '/cart',
       templateUrl: 'views/cart.html',
