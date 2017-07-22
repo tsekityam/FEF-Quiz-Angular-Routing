740e01ed60475c771e137d928077a6ad89621574
Tse Kit Yam <me@kytse.com>
2017-07-22 23:18:58 +0800

    Create a state for cart template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,12 @@ angular
   .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/');
 
+    $stateProvider.state('cart', {
+      url: '/cart',
+      templateUrl: 'views/cart.html',
+      controller: 'CartCtrl as cart'
+    });
+
     $stateProvider.state('home', {
       url: '/',
       templateUrl: 'views/instructions.html'
